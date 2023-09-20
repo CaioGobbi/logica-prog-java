cbba37abe0d05ad7732c695396109111d1bdd026
Caio Gobbi <caio.cameyo@gmail.com>
2023-09-20 08:52:14 -0300

    Repetição e Decisão juntos

--- src/main/java/logica/basica/EstruturaDeRepeticao.java
+++ src/main/java/logica/basica/EstruturaDeRepeticao.java
@@ -6,6 +6,8 @@ public class EstruturaDeRepeticao {
     public static void main(String[] args) {
         ListaDePalavras();
         forClassico();
+        forPuro();
+        inverteString();
     }
 
     private static void ListaDePalavras() {
@@ -19,6 +21,7 @@ public class EstruturaDeRepeticao {
         for (String palavra : palavraList) {
             System.out.println(palavra);
         }
+        palavraList.forEach(palavra -> System.out.println(palavra));
     }
     private static void forClassico (){
         ArrayList<String> palavraList = new ArrayList<>();
@@ -31,5 +34,19 @@ public class EstruturaDeRepeticao {
         for (int i = 0; i < palavraList.size(); i++){
             System.out.println(palavraList.get(i));
         }
+    }
+    private static void forPuro(){
+        for(int posicao = 0; posicao <= 10; posicao = posicao + 1){
+            System.out.println(posicao);
+        }
+    }
+    private static void inverteString() {
+        String frase = "Oi meu nome é Caio";
+        String invertida = "";
+
+        for (int posicao = frase.length() - 1; posicao >= 0; posicao = posicao - 1) {
+            invertida = invertida + frase.charAt(posicao);
+        }
+        System.out.println(invertida);
     }
 }
